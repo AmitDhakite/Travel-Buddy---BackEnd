d0700ad8ee40b9b7b70d23deb6612593e0d6ba8d
Amit Dhakite <amitdhakite24@gmail.com>
2021-07-28 15:09:33 +0530

    MyAccount

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -35,3 +35,42 @@ export const getUsers = (req, res) => {
     res.status(500).json(e);
   }
 };
+
+export const getUserById = (req, res) => {
+  try {
+    User.findOne({ _id: req.body.userId }, (err, foundUser) => {
+      if (!err) {
+        res.status(200).json(foundUser);
+      } else {
+        res.status(400).json(err);
+      }
+    });
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
+
+export const editUser = (req, res) => {
+  try {
+    const { user, userId } = req.body;
+    User.findOne({ _id: userId }, (err, foundUser) => {
+      if (!err) {
+        foundUser.firstName = user.firstName;
+        foundUser.lastName = user.lastName;
+        foundUser.mobile = user.mobile;
+        foundUser.addressLine1 = user.addressLine1;
+        foundUser.addressLine2 = user.addressLine2;
+        foundUser.city = user.city;
+        foundUser.state = user.state;
+        foundUser.zipCode = user.zipCode;
+        foundUser.country = user.country;
+        foundUser.save();
+        res.status(200).json(foundUser);
+      } else {
+        res.status(400).json(err);
+      }
+    });
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,16 @@
 import express from "express";
 const router = express.Router();
 
-import { register, getUsers } from "../controllers/user.controller.js";
+import {
+  register,
+  getUsers,
+  getUserById,
+  editUser,
+} from "../controllers/user.controller.js";
 
 router.post("/register", register);
 router.get("/register", getUsers);
+router.post("/getUser", getUserById);
+router.post("/editUser", editUser);
 
 export default router;
