5d7f67a9d1f4338d1fce72ee7c1c06aca6b78f08
Amit Dhakite <amitdhakite24@gmail.com>
2021-07-30 15:58:00 +0530

    New Trip

---
+++ models/trip.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const tripSchema = new Schema({
+  userId: { type: String },
+  from: { type: String },
+  to: { type: String },
+  startDate: { type: Date },
+  endDate: { type: Date },
+  twoWay: { type: Boolean },
+  transport: { type: String },
+});
+
+const Trip = mongoose.model("Trip", tripSchema);
+
+export default Trip;
